c2654688f734039bd128ad64fa90982e1d7e49cf
potatoes <osoriomando2345@gmail.com>
2022-09-20 10:27:01 -0700

    fixed saved books page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,67 +7,70 @@ const { signToken } = require("../utils/auth")
 // Step 15: resolve/create code for mutations included in typeDefs file (mutations modify database)
 const resolvers = {
     Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.fineOne({ _id: context.user._id }).select("-__v -password");
-                return userData;
-            }
-            throw new AuthenticationError("Not logged in");
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+  
+          return userData;
         }
+  
+        throw new AuthenticationError('Not logged in');
+      },
     },
-
+  
     Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return { token, user };
-        },
-
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-
-            if(!user) {
-                throw new AuthenticationError("Incorrect credentials");
-            }
-
-            const correctPw = await user.isCorrectPassword(password);
-
-            if(!correctPw) {
-                throw new AuthenticationError("Incorrect Credentials");
-            }
-
-            const token = signToken(user);
-            return { token, user };
-        },
-
-        saveBook: async (parent, { bookData }, context) => {
-            if(context.user) {
-                const updatedUser = await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { savedBooks: bookData } },
-                    { new: true }
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError("You need to be logged in!");
-        },
-
-        removeBook: async (parent, { bookId }, context) => {
-            if(context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId } } },
-                    { new: true }
-                );
-
-                return updatedUser;
-            }
-            throw new AuthenticationError("You need to be logged in!");
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
         }
-    }
-};
-
-module.exports = resolvers;
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      },
+      saveBook: async (parent, { bookData }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: bookData } },
+            { new: true }
+          );
+  
+          return updatedUser;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      },
+      removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId } } },
+            { new: true }
+          );
+  
+          return updatedUser;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+  };
+  
+  module.exports = resolvers;
 
 // Step 16: routes folder can now be removed as well as controllers
 // Step 17: start working on client side
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,47 +4,47 @@ const { gql } = require("apollo-server-express");
 //whatever comes after the colon in the mutation is the type that is being returned (type User is being returned in saveBook and removeBook)
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String
-        image: String
-        link: String
-        title: String!
-    }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    input BookInput {
-        authors: [String]
-        description: String!
-        bookId: String!
-        image: String!
-        link: String
-        title: String!
-    }
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Query {
-        me: User
-    }
+  type Query {
+    me: User
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: ID!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
